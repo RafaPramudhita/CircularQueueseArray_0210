9869aa36c672b596a34da73b9b3625aca9252008
RafaPramudhita <161433424+RafaPramudhita@users.noreply.github.com>
2024-05-30 14:51:55 +0700

    membuat variabel global

--- CircularQueueseArray.cpp
+++ CircularQueueseArray.cpp
@@ -1,14 +1,4 @@
 #include <iostream>
 using namespace std;
 
-class Queues {
-    int FRONT, REAR, max = 5;
-    int queue_array[5];
 
-public:
-    Queues() {
-        FRONT = -1;
-        REAR = -1;
-    }
-
-}
