f13302af5d2caab8df7035c6f58d73a03a14e41b
RafaPramudhita <161433424+RafaPramudhita@users.noreply.github.com>
2024-05-30 15:01:58 +0700

    buat kondisi jika cek apakah antrian kosong

--- CircularQueueseArray.cpp
+++ CircularQueueseArray.cpp
@@ -17,6 +17,13 @@ public:
         cin >> num;
         cout << endl;
 
+        //Cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
+       
     }
    
 };
